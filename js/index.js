a7261862ab325d7a6531bbd80109c8e6f50d5494
Unredix <999daved@gmail.com>
2025-01-20 23:21:09 +0100

    New menu on homepage, some animations

---
+++ js/index.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      } else {
+        entry.target.classList.remove("visible");
+      }
+    });
+  });
+
+  const hiddenElements = document.querySelectorAll(".hidden");
+  hiddenElements.forEach((element) => {
+    observer.observe(element);
+  });
+});
