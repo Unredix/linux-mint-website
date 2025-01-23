c201b05158629c14b21598d98eec93191f6c8a32
Unredix <999daved@gmail.com>
2025-01-23 22:42:28 +0100

    Animations, picture slide, ect...

--- js/index.js
+++ js/index.js
@@ -13,4 +13,51 @@ document.addEventListener("DOMContentLoaded", function () {
   hiddenElements.forEach((element) => {
     observer.observe(element);
   });
+
+  let sources = [
+    "assets/main.png",
+    "assets/xfce.png",
+    "assets/mate.png",
+    "assets/myscreen.png",
+  ];
+
+  let current = 0;
+
+  document.getElementById("prev").onclick = function () {
+    changeImg(-1);
+  };
+  document.getElementById("next").onclick = function () {
+    changeImg(1);
+  };
+
+  function changeImg(num) {
+    const mainImg = document.getElementById("main");
+    const prevImg = document.getElementById("prev-img-js");
+    const nextImg = document.getElementById("next-img-js");
+
+    // Add animation classes
+    mainImg.classList.add("img-change-from-main");
+    prevImg.classList.add("img-change-left");
+    nextImg.classList.add("img-change-right");
+
+    // Wait for the animation to complete
+    setTimeout(() => {
+      current += num;
+      if (current < 0) {
+        current = sources.length - 1;
+      }
+      if (current >= sources.length) {
+        current = 0;
+      }
+
+      mainImg.src = sources[current];
+      prevImg.src = sources[(current - 1 + sources.length) % sources.length];
+      nextImg.src = sources[(current + 1) % sources.length];
+
+      // Remove animation classes
+      mainImg.classList.remove("img-change-from-main");
+      prevImg.classList.remove("img-change-left");
+      nextImg.classList.remove("img-change-right");
+    }, 500); // Adjust the timeout to match the animation duration
+  }
 });
